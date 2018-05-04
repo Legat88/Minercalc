7ff5f3cd5b845576c0856b625072ddfa0cf1af88
legat88 <legat88@gmail.com>
2018-05-05 00:40:27 +0500

    origin+add rpc

--- scripts/difficulty.php
+++ scripts/difficulty.php
@@ -1,25 +1,38 @@
 <?php
 require "db.php";
-$stmt=$dbh->query("SELECT * FROM coins");
-while ($result=$stmt->fetch(PDO::FETCH_LAZY)) {
-    $url=$result->url;
-    if (!$data=file_get_contents($url)) {
-        continue;
-    } else {
-        $coin[]=$result->code;
-        $parameter=$result->parameter;
-        $addition=$result->addition;
-        $info=json_decode($data);
-        if ($addition !=NULL && $parameter!=NULL) {
-            $difficulty[]=$info->$addition->$parameter;
-        } elseif ($addition==NULL && $parameter!=NULL) {
-            $difficulty[]=$info->$parameter;
-        } elseif ($addition==NULL && $parameter==NULL) {
-            $difficulty[]=$info;
+require_once('easybitcoin.php');
+$stmt = $dbh->query("SELECT * FROM coins");
+while ($result = $stmt->fetch(PDO::FETCH_LAZY)) {
+    $rpc_mode = $result->rpc;
+    if ($rpc_mode == 0) {
+        $url = $result->url;
+        if (!$data = file_get_contents($url)) {
+            continue;
+        } else {
+            $coin[] = $result->code;
+            $parameter = $result->parameter;
+            $addition = $result->addition;
+            $info = json_decode($data);
+            if ($addition != NULL && $parameter != NULL) {
+                $difficulty[] = $info->$addition->$parameter;
+            } elseif ($addition == NULL && $parameter != NULL) {
+                $difficulty[] = $info->$parameter;
+            } elseif ($addition == NULL && $parameter == NULL) {
+                $difficulty[] = $info;
+            }
         }
+    } else {
+        $coin[] = $result->code;
+        $rpcuser = $result->rpcuser;
+        $rpcpassword = $result->rpcpassword;
+        $rpcport = $result->rpcport;
+        $rpc = new Bitcoin($rpcuser, $rpcpassword, 'localhost', $rpcport);
+        $rpc->getinfo();
+        $difficulty[] = $rpc->response['result']['difficulty'];
     }
+
 }
-$coins=implode(', ', $coin);
-$difficulties=join(', ', $difficulty);
+$coins = implode(', ', $coin);
+$difficulties = join(', ', $difficulty);
 $dbh->query("INSERT INTO difficulty (datetime, $coins) VALUES (NOW(), $difficulties)");
 ?>
