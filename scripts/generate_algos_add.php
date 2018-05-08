7502b98b34ab4080647b0ac9e3d026f834038053
Legat88 <legat88@gmail.com>
2018-05-08 09:26:23 +0500

    input number min=1 to generate algos

--- scripts/generate_algos_add.php
+++ scripts/generate_algos_add.php
@@ -5,7 +5,7 @@ while ($algo=$stmt->fetch(PDO::FETCH_LAZY)) {
     echo '<div class="'.$css_grid.'">';
     echo '<h6 class="text-center">'.$algo->name.'</h6>';
     echo '<div class="input-group">';
-    echo '<input type="number" class="form-control add" name="' . $algo->name . '" id="' . $algo->name . '" value="">';
+    echo '<input type="number" class="form-control add" name="' . $algo->name . '" id="' . $algo->name . '" value="" min="0">';
     echo '<div class="input-group-append">';
     echo '<span class="input-group-text">'.$algo->measure.'</span>';
     echo '</div>';
