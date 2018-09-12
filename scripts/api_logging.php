58ba2ef7d176945cc9c0e93997fc5b6aa20d44e8
Alexey Bogdanov <legat88@gmail.com>
2018-09-12 12:40:30 +0500

    fix path to log file

--- scripts/api_logging.php
+++ scripts/api_logging.php
@@ -1,7 +1,7 @@
 <?php
 function api_error($textLog)
 {
-    $file = $_SERVER['DOCUMENT_ROOT'] . '/logs/api_errors.log';
+    $file = __DIR__ . '/../logs/api_errors.log';
     $text = "=======================\n"; // Обязательно двойные кавычки, в одинарных не будет работать перевод строки
     $text .= $textLog;//Выводим переданную переменную
     $text .= "\n" . date('Y-m-d H:i:s') . "\n"; //Добавим актуальную дату после текста или дампа массива
